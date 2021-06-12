10f6af32e3b0d9b2e6f4bfdb92f294f36948171b
dikawiniminiwinibiti <mahardikarahman@gamil.com>
2021-06-12 15:39:02 +0700

    ui

---
+++ app/src/main/java/com/example/nba/view/RegisterActivity.java
@@ -0,0 +1,16 @@
+package com.example.nba.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.nba.R;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
